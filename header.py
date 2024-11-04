af584a635c845b66c7eebcf170d911b1c2091685
hey537816 <youh6106@gmail.com>
2024-11-04 14:30:58 -0300

    Create header.py

---
+++ header.py
@@ -0,0 +1,15 @@
+# header.py
+
+def print_header():
+    print("      __                _      _         ")
+    print("     / _|              (_)    | |        ")
+    print("    | |_ ___  ___   ___ _  ___| |_ _   _ ")
+    print("    |  _/ __|/ _ \ / __| |/ _ \ __| | | |")
+    print("    | | \__ \ (_) | (__| |  __/ |_| |_| |")
+    print("    |_| |___/\___/ \___|_|\___|\__|\__, |")
+    print("                                    __/ |")
+    print("                                   |___/ ")
+
+# Aqui você pode definir a função `put`, se necessário
+def put():
+    print("Função put chamada.")
